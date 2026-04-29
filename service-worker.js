bdca2e305db0c3159e4d48dfe4cccaec362f8d08
mateogarcia2170-boop <mateo.garcia.2170@gmail.com>
2026-04-29 15:26:30 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,3 @@
 self.addEventListener("install", e => {
-  console.log("SW listo");
-});
-
-self.addEventListener("fetch", e => {
-  // NO intercepta nada (evita pantalla blanca)
+  console.log("SW activo");
 });
