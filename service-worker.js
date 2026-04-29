b03710193f04a0eef597a72503f7fc9840284516
mateogarcia2170-boop <mateo.garcia.2170@gmail.com>
2026-04-29 14:51:04 -0500

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,21 @@
-self.addEventListener("install", e => {
-    console.log("App instalada");
+const CACHE_NAME = "hielo-app-v1";
+
+const urlsToCache = [
+  "index.html",
+  "tienda.html",
+  "logo.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
